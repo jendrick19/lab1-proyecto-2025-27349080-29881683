378d0658e220793726275aaefb1021e77bb208f2
jendrick <jendrickmontiel18@gmail.com>
2025-11-07 19:45:50 -0400

    feat: Create model PersonaAtendida

--- config/config.js
+++ config/config.js
@@ -1,11 +1,8 @@
-// config/config.js (VERSIÓN CORREGIDA)
-
-// 1. Cargamos las variables del archivo .env
 require('dotenv').config();
 
 module.exports = {
   development: {
-    // Accedemos a process.env directamente para la máxima compatibilidad
+
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -14,19 +11,19 @@ module.exports = {
     dialect: "mysql"
   },
   test: {
-    // Usaríamos una base de datos diferente para testing, pero mantenemos el mismo dialecto
+
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST || 'lab1_medicos_test', // Ejemplo de test DB
+    database: process.env.DB_NAME_TEST || 'clinica', // Ejemplo de test DB
     host: process.env.DB_HOST,
     dialect: "mysql"
   },
   production: {
-    // La configuración de producción, que usaría diferentes variables de entorno
+  
     username: process.env.DB_USERNAME_PROD,
     password: process.env.DB_PASSWORD_PROD,
     database: process.env.DB_NAME_PROD,
     host: process.env.DB_HOST_PROD,
-    dialect: "mysql"
-  }
+    dialect: "mysql"
+  }
 };
